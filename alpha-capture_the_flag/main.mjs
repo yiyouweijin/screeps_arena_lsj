c9e8cf04b39071e577742d1b3f5f446c91da5bf0
yiyouweijin <295051942@qq.com>
2024-04-29 10:34:49 +0800

    ctf

---
+++ alpha-capture_the_flag/main.mjs
@@ -0,0 +1,10 @@
+import { findClosestByPath, getObjectsByPrototype,
+     getTicks, getRange, findInRange } from 'game/utils';
+import {Creep, StructureSpawn, Source, StructureContainer, Structure,
+    OwnedStructure, StructureTower, StructureRampart} from 'game/prototypes';
+import { MOVE, ATTACK, HEAL, RANGED_ATTACK, 
+    WORK, CARRY, RESOURCE_ENERGY, ERR_NOT_IN_RANGE } from 'game/constants';
+
+export function loop() {
+    // Your code goes here
+}

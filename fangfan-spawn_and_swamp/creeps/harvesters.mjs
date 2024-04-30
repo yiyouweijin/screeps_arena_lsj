afb7d1a45f1dd04a6c82e3beb7990afde6825fff
yiyouweijin <295051942@qq.com>
2024-04-30 11:18:45 +0800

    重构采集者

---
+++ fangfan-spawn_and_swamp/creeps/harvesters.mjs
@@ -0,0 +1,50 @@
+import { findClosestByPath, getObjectsByPrototype, getTicks, getRange, findInRange } from 'game/utils';
+import {
+    Creep, StructureSpawn, Source, StructureContainer, Structure,
+    OwnedStructure, StructureTower, StructureRampart
+} from 'game/prototypes';
+import { MOVE, ATTACK, HEAL, RANGED_ATTACK, WORK, CARRY, RESOURCE_ENERGY, ERR_NOT_IN_RANGE } from 'game/constants';
+
+export class Harvest {
+    /**
+     * @param {Creep[]} myCreeps
+     ** @param {StructureSpawn} mySpawn
+     */
+    constructor(myCreeps, mySpawn, ) {
+        this.mySpawn = mySpawn;
+        this.my_harvests = myCreeps.filter(creep => creep.zhiye == 'harvester');
+        this.number = this.my_harvests.length;
+
+    }
+
+    /**
+     * @param {number} hopefule_number
+     */
+    work(hopefule_number){
+        var sources = getObjectsByPrototype(StructureContainer).filter(s => s.store.getUsedCapacity(RESOURCE_ENERGY) > 5);
+        if (!findInRange(this.mySpawn,sources,6)){
+            hopefule_number = 0
+        }
+        if (this.my_harvests.length < hopefule_number) {
+            var hc = this.mySpawn.spawnCreep([CARRY, MOVE]).object
+            if (hc) {
+                hc.zhiye = 'harvester'
+            }
+        }
+        // 采集能量
+        for (var my_harvest of this.my_harvests) {
+            if (my_harvest.store.getFreeCapacity(RESOURCE_ENERGY)) {
+                // find near container
+                let source = findClosestByPath(my_harvest, sources)
+                if (my_harvest.withdraw(source, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    my_harvest.moveTo(source);
+                }
+            } else {
+                if (my_harvest.transfer(this.mySpawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    my_harvest.moveTo(this.mySpawn);
+                }
+            }
+        }
+    }
+
+}
--- fangfan-spawn_and_swamp/main.mjs
+++ fangfan-spawn_and_swamp/main.mjs
@@ -1,4 +1,5 @@
-import { Youxias } from './zhanshi/youxia.mjs';
+import { Youxias } from './creeps/youxia.mjs';
+import { Harvest } from './creeps/harvesters.mjs';
 
 import { findClosestByPath, getObjectsByPrototype, getTicks, getRange, findInRange } from 'game/utils';
 import {
@@ -18,42 +19,26 @@ export function loop() {
     let costs = new CostMatrix;
     console.log(costs)
 
-    var sources = getObjectsByPrototype(StructureContainer).filter(s => s.store.getUsedCapacity(RESOURCE_ENERGY) > 50);
+    
     // console.log(sources)
 
     // 判断我的所有单位
     var myCreeps = getObjectsByPrototype(Creep).filter(creep => creep.my);
-    var my_harvests = myCreeps.filter(creep => creep.zhiye == 'harvester');
     var my_zhanshi = myCreeps.filter(creep => creep.zhiye == 'zhanshi');
     var my_hurt_creeps = myCreeps.filter(creep => (creep.hits < creep.hitsMax - 20))
 
     var mySpawn = getObjectsByPrototype(StructureSpawn).find(s => s.my);
     var my_all_youxia = new Youxias(my_zhanshi,mySpawn)
+
     // 生产
-    if (my_harvests.length < 4) {
-        var hc = mySpawn.spawnCreep([CARRY, MOVE]).object
-        if (hc) {
-            hc.zhiye = 'harvester'
-        }
-    }
-    my_all_youxia.create(30)  // 生产战士
+    var my_harvest = new Harvest(myCreeps, mySpawn)
+    my_harvest.work(3)
+
+    my_all_youxia.create(20)  // 生产战士
 
 
     // console.log('line 31')
-    // 采集能量
-    for (var my_harvest of my_harvests) {
-        if (my_harvest.store.getFreeCapacity(RESOURCE_ENERGY)) {
-            // find near container
-            let source = findClosestByPath(my_harvest, sources)
-            if (my_harvest.withdraw(source, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                my_harvest.moveTo(source);
-            }
-        } else {
-            if (my_harvest.transfer(mySpawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                my_harvest.moveTo(mySpawn);
-            }
-        }
-    }
+
     // console.log('line 46')
     // 攻击
     var enemyCreeps = getObjectsByPrototype(Creep).filter(creep => !creep.my);
